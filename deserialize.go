020a1710ff6707de5f3888b4369ca2c0981a9dee
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2019-02-02 22:26:41 -0800

    added deserialization dsl

---
+++ deserialize.go
@@ -0,0 +1,41 @@
+package graph
+
+// Property is an arbitrary name value pair
+type Property struct {
+	Name  string
+	Value string
+}
+
+// Resource models a virtual service
+type Resource struct {
+	Name       string
+	Type       string
+	Properties []Property
+	DependsOn  []string
+}
+
+// BuildGraph converts resource table to Graph
+func BuildGraph(resources []*Resource) *Graph {
+	parents := map[int][]int{}
+	indexes := map[string]int{}
+
+	for i := range resources {
+		indexes[resources[i].Name] = i
+	}
+
+	for i := range resources {
+		for _, dep := range resources[i].DependsOn {
+			parents[i] = append(parents[i], indexes[dep])
+		}
+	}
+
+	g := New(len(resources))
+
+	for w, arr := range parents {
+		for _, v := range arr {
+			g.AddEdge(v, w)
+		}
+	}
+
+	return g
+}
---
+++ deserialize_test.go
@@ -0,0 +1,30 @@
+package graph
+
+import "testing"
+
+func TestBuildGraph(t *testing.T) {
+
+	resources := []*Resource{{
+		Name: "mykin",
+		Type: "kinesis",
+	}, {
+		Name: "mydyn",
+		Type: "dynamo",
+	}, {
+		Name:      "mydep1",
+		Type:      "deployment",
+		DependsOn: []string{"mykin"},
+	}}
+
+	g := BuildGraph(resources)
+
+	t.Logf("graph = %v\n", g)
+
+	if g.Vertices() != 3 {
+		t.Fatal("vertices incorrect")
+	}
+
+	if len(g.Adjascent(0)) != 1 || g.Adjascent(0)[0] != 2 {
+		t.Fatal("incorrect edges")
+	}
+}
