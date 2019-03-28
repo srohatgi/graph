d57db22afa3eee7d293d5a1e6423677a5c942aa3
Mehul Goyal <mehulg@splunk.com>
2019-03-28 16:38:18 -0700

    ensure all dependent resources exist, otherwise throw an error

--- builder_test.go
+++ builder_test.go
@@ -77,5 +77,26 @@ func TestSync(t *testing.T) {
 	if status["mydep1"] != arn {
 		t.Fatal("expected mydep1 status to return kinesis arn")
 	}
+}
 
+func TestInvalidDependency(t *testing.T) {
+
+	mykin := "mykin"
+
+	ctxt := context.Background()
+
+	arn := "hello123"
+
+	kinesisResource := MakeResource(mykin, nil, &kinesis{ctxt, arn}, func(x interface{}) (string, error) { return "", nil }, func(x interface{}) error { return nil })
+	deploymentResource := MakeResource("mydep1", []Dependency{{"mykin2", "Arn", "KinesisArn"}}, &deployment{ctxt: ctxt}, func(x interface{}) (string, error) { d := x.(*deployment); return d.KinesisArn, nil }, func(x interface{}) error { return nil })
+
+	resources := []Resource{kinesisResource, deploymentResource}
+
+	lib := New(&Opts{CustomLogger: t.Log})
+
+	_, err := lib.Sync(ctxt, resources, false)
+
+	if err == nil {
+		t.Fatalf("expected sync to fail with invalid dependency")
+	}
 }
