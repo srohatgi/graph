514878f33ebcaa87696bb18bb7d4f7c8c9fa7ed6
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2019-05-12 09:03:19 -0700

    fleshed out example with retries

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package graph_test
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	yaml "gopkg.in/yaml.v2"
 
@@ -38,6 +39,7 @@ type factory struct {
 	Kinesis    []*Kinesis
 	Deployment []*Deployment
 	Dynamo     []*Dynamo
+	kb         *kinesisBuilder
 }
 
 func new(data string) (*factory, error) {
@@ -54,7 +56,7 @@ func new(data string) (*factory, error) {
 func (f *factory) build() []graph.Resource {
 	resources := []graph.Resource{}
 	for _, k := range f.Kinesis {
-		resources = append(resources, k)
+		resources = append(resources, k.WithBuilder(f.kb))
 	}
 	for _, d := range f.Dynamo {
 		resources = append(resources, d)
@@ -90,6 +92,8 @@ func Example_usage() {
 		fmt.Printf("error creating factory: %v\n", err)
 	}
 
+	f.kb = buildKinesisBuilder()()
+
 	envFactory, err := new(overrideSpec)
 	if err != nil {
 		fmt.Printf("error creating override factory: %v\n", err)
@@ -107,24 +111,35 @@ func Example_usage() {
 
 	lib := graph.New(&graph.Opts{CustomLogger: myprint})
 
-	//fmt.Printf("factory: %v\n", f)
+	// fmt.Printf("factory: %v\n", f)
 
 	ctxt := context.WithValue(context.Background(), graph.SyncBag, map[string]string{"namespace": "myns"})
 
-	status, err := lib.Sync(ctxt, resources, false)
-	if err != nil {
-		if em, ok := err.(graph.ErrorMapper); ok {
-			for resourceName, err := range em.ErrorMap() {
-				fmt.Printf("resource %s creation had error %v\n", resourceName, err)
+	var status map[string]string
+	var syncError error
+
+	for {
+		status, syncError = lib.Sync(ctxt, resources, false)
+		if syncError != nil {
+			if em, ok := syncError.(graph.ErrorMapper); ok {
+				for resourceName, err := range em.ErrorMap() {
+					fmt.Printf("resource %s creation had error %v\n", resourceName, err)
+				}
+			} else {
+				fmt.Printf("unable to sync resources, error = %v\n", err)
+				return
 			}
 		} else {
-			fmt.Printf("unable to sync resources, error = %v\n", err)
+			fmt.Println("successfully synced all resources")
+			break
 		}
 	}
 
 	fmt.Printf("kinesis status = %s\n", status["mykin"])
 	fmt.Printf("deployment status = %s\n", status["mydep"])
 	// Output:
+	// resource mykin creation had error kinesis stream not ready yet
+	// successfully synced all resources
 	// kinesis status = successfully created stream myEventStream with 10 shards
 	// deployment status = successfully reading from stream arn hello123 in myns
 }
@@ -135,11 +150,48 @@ type Kinesis struct {
 	ShardCount    int
 	StreamName    string
 	Arn           string
+	kb            *kinesisBuilder
+}
+
+type kinesisBuilder struct {
+	tries int
+	mux   *sync.Mutex
+}
+
+func (kb *kinesisBuilder) ready() bool {
+	kb.mux.Lock()
+	defer kb.mux.Unlock()
+	kb.tries++
+	return kb.tries > 1
+}
+
+var once sync.Once
+
+func buildKinesisBuilder() func() *kinesisBuilder {
+	var kb *kinesisBuilder
+	once.Do(func() {
+		kb = &kinesisBuilder{
+			mux: &sync.Mutex{},
+		}
+	})
+
+	return func() *kinesisBuilder {
+		return kb
+	}
+}
+
+func (kin *Kinesis) WithBuilder(kb *kinesisBuilder) *Kinesis {
+	kin.kb = kb
+	return kin
 }
 
 func (kin *Kinesis) Update(ctxt context.Context) (string, error) {
 	kin.Arn = "hello123"
-	return fmt.Sprintf("successfully created stream %s with %d shards", kin.StreamName, kin.ShardCount), nil
+	if kin.kb.ready() {
+		return fmt.Sprintf("successfully created stream %s with %d shards", kin.StreamName, kin.ShardCount), nil
+	}
+
+	return "", fmt.Errorf("kinesis stream not ready yet")
 }
 func (kin *Kinesis) Delete(ctxt context.Context) error {
 	return nil
